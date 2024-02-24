f267a121ae8a4ca584c803452c791c34a9970928
lz-junior <correajr.dev@gmail.com>
2024-02-24 20:52:48 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Tasks</h1>
+      <Outlet/>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// pages
+import Login from './pages/Login/login.tsx';
+import Home from './pages/Home/home.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <Login/>,
+      },
+      {
+        path: "/",
+        element: <Home/>,
+      },
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
