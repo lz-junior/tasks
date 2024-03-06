5c23baeba3726c92e590e5a7b752bcc89e5ec049
lz-junior <correajr.dev@gmail.com>
2024-03-06 17:07:00 -0300

    teste de código em Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,37 @@
+import React from 'react';
 import Header from '../../components/Header';
+import { useState } from 'react';
 import './home.css';
 
 
-const Home = ()=> {
+interface AddTaskFormProps {
+  onAddTask: (task: string)=> void;
+}
+
+
+
+const Home: React.FC<AddTaskFormProps> = ({ onAddTask })=> {
+  const [task, setTask] = useState('');
+
+  
+  const handleAddTask = ()=> {
+    if(task.trim() !== '') {
+      onAddTask(task);
+      setTask('');
+    }
+  }
+  
+
   return (
     <div>
       <Header/>
-      <h1>Home</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e)=> setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={handleAddTask} >Add</button>
     </div>
   )
 };
