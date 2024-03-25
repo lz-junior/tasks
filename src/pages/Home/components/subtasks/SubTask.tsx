8d2a0c2e621e37906cb29e5b754679f05a5e8a3f
lz-junior <correajr.dev@gmail.com>
2024-03-25 19:52:28 -0300

    subtask started

--- src/pages/Home/components/subtasks/SubTask.tsx
+++ src/pages/Home/components/subtasks/SubTask.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
+
+// interface SubtaskProps {
+//   subtask: string;
+// }
 
 const SubTask = ()=> {
+  const [task, setTask] = useState<any>();
+  const addSubtask = ()=> {
+
+  }
+
+
   return (
     <div>
       <h4>SubTask</h4>
+      <ul>
+        {task && task.map((task: any, index: any)=> {
+          <li>
+            <input type="checkbox" key={index}/>
+            <input type="text" key={index} value={task}/>
+          </li>
+        })}
+      </ul>
+
+      <form onSubmit={addSubtask}>
+        <input 
+          type="text"
+          placeholder="add new subtask"
+          value={task}
+          onChange={(e)=> setTask(e.target.value)}
+        />
+        <button type="submit">+</button>
+      </form>
     </div>
   )
 }
