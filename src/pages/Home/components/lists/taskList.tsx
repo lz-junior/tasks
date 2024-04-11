6e92a79a53e89c8536c984f9288957bb7c9b7dbc
lz-junior <correajr.dev@gmail.com>
2024-04-11 18:59:07 -0300

    joining code

--- src/pages/Home/components/lists/taskList.tsx
+++ src/pages/Home/components/lists/taskList.tsx
@@ -1,6 +1,9 @@
 // src/components/TaskList.tsx
-import React from 'react';
-import ListItem from './ListItem';
+import React, { useState } from 'react';
+// import ListItem from './ListItem';
+import classes from './listItem.module.css'
+import Tasks from '../tasks/Tasks';
+
 
 
 
@@ -12,10 +15,46 @@ interface TaskListProps {
 
 
 const TaskList: React.FC<TaskListProps> = ({ tasks })=> {
+
+  const [isChecked, setIsChecked] = useState(false);
+  const [openTasks, setOpenTasks] = useState(false);
+  const [task, setTask] = useState<string[]>([]);
+
+  const handleCheckboxChange = ()=> {
+    setIsChecked(!isChecked);
+  }
+  const openTask = ()=> {
+    setOpenTasks(!openTasks);
+  }
+  const handleAddTask = (newTask: string)=> {
+    setTask(prevTask => [...prevTask, newTask])
+  }
+
   return (
     <ul>
       {tasks.map((task, index)=> (
-        <ListItem key={index} task={task}/>
+        <div>
+        <div className={classes.container}>
+          <input
+            type="checkbox"
+            className={classes.checkbox}
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <button 
+            style={{ 
+              textDecoration: isChecked ? 'line-through' : 'none',
+              opacity: isChecked ? '0.4' : '1' 
+            }}
+            onClick={openTask}>
+              {task}
+          </button>
+        </div>
+  
+        <div className={classes.tasksOpened}>
+          {openTasks && <Tasks saveTask={tasks} onAddTask={handleAddTask} />}
+        </div>
+      </div>
       ))}
     </ul>
   );
