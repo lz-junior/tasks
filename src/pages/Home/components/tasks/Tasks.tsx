f3f3402f87e13877e0b74b3d6bb3fee61b0cffd2
lz-junior <correajr.dev@gmail.com>
2024-04-24 11:47:50 -0300

    handleAddTask

--- src/pages/Home/components/tasks/Tasks.tsx
+++ src/pages/Home/components/tasks/Tasks.tsx
@@ -1,6 +1,6 @@
-import React, {  } from "react";
-// import { IoIosAddCircleOutline  } from "react-icons/io";
+import React, { useState } from "react";
 import classes from "./tasks.module.css"
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 
 interface TaskProps {
@@ -9,14 +9,28 @@ interface TaskProps {
 
 
 const Tasks: React.FC<TaskProps> = ()=> {
+  const [tasks, setTasks] = useState<string>('');
 
+  const handleAddTask = ()=> {
+
+  }
 
 
   return (
     <div className={classes.containerTasks}>
       <h3>Tasks</h3>
-
-      
+      <ul>
+        {tasks}
+      </ul>
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          placeholder="add a new list"
+          value={tasks}
+          onChange={(e)=> setTasks(e.target.value)}
+        />
+        <button type="submit"><IoIosAddCircleOutline/></button>
+      </form>
     </div>
   );
 }
