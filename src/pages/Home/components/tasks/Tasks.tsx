54dfe3807a9624f2f7b7d543f7de362efd57bb3d
lz-junior <correajr.dev@gmail.com>
2024-04-25 17:54:14 -0300

    correcao

--- src/pages/Home/components/tasks/Tasks.tsx
+++ src/pages/Home/components/tasks/Tasks.tsx
@@ -8,19 +8,21 @@ import { AppDispatch } from "../../../../redux/store";
 
 
 interface TaskProps {
-  
+  initialTasks?: string[];
 }
 
 
-const Tasks: React.FC<TaskProps> = ()=> {
-  const [tasks, setTasks] = useState('');
+const Tasks: React.FC<TaskProps> = ({ initialTasks = [] })=> {
+  const [taskInput, setTaskInput] = useState('');
+  const [tasks, setTasks] = useState<string[]>(initialTasks);
   const dispatch = useDispatch<AppDispatch>();
 
   const handleAddTask = (e:any)=> {
     e.preventDefault();
-    if (tasks.trim()) {
-      dispatch(addTask(tasks))
-      setTasks('');
+    if (taskInput.trim()) {
+      dispatch(addTask(taskInput));
+      setTasks([...tasks, taskInput]);
+      setTaskInput('');
     }
   }
 
@@ -30,17 +32,16 @@ const Tasks: React.FC<TaskProps> = ()=> {
       <h3>Tasks</h3>
       
       <ul>
-        {tasks.map((task:string)=> {
-          <li>{task}</li>
-
+        {tasks.map((task:string, index:number)=> {
+          return <li key={index}>{task}</li>
         })}
       </ul>
       <form onSubmit={handleAddTask}>
         <input
           type="text"
           placeholder="add a new list"
-          value={tasks}
-          onChange={(e)=> setTasks(e.target.value)}
+          value={taskInput}
+          onChange={(e)=> setTaskInput(e.target.value)}
         />
         <button type="submit"><IoIosAddCircleOutline/></button>
       </form>
